39c5129b80d85fcae5e1bb0a3bbefb98039bad79
takuya_kajiwara <fd.elt.119@gmail.com>
2023-07-20 22:29:53 +0900

    新規登録api実装

--- api/server.js
+++ api/server.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+const { PrismaClient } = require("@prisma/client");
 
 const PORT = 8001;
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+//新規ユーザー登録api
+app.post("/api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10); //hash化
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return res.json({ user });
+});
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
