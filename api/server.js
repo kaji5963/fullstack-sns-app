1e78294e1a95abab8e686c89c0e0ca57a4e81bfd
takuya_kajiwara <fd.elt.119@gmail.com>
2023-07-22 21:01:04 +0900

    cors導入、新規登録時のcorsエラー解消

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -8,6 +9,9 @@ const PORT = 8001;
 
 // json()のuseがないとリクエストできない
 app.use(express.json());
+
+// corsエラーに対しuseする
+app.use(cors());
 
 // auth.jsのrouterをuseする
 app.use("/api/auth", authRoute);
