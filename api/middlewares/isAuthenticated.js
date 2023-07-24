6af86845e10afa5545ffe63e0ebd8dcc4dbb5417
takuya_kajiwara <fd.elt.119@gmail.com>
2023-07-24 12:48:48 +0900

    ログアウト機能実装、ログイン時のバグ修正

--- api/middlewares/isAuthenticated.js
+++ api/middlewares/isAuthenticated.js
@@ -4,14 +4,13 @@ const jwt = require("jsonwebtoken");
 function isAuthenticated(req, res, next) {
   // フロント側の定数`Bearer ${token}`をsplitで半角で区切り配列、要素の１番目を取得
   const token = req.headers.authorization?.split(" ")[1];
-
   if (!token) {
-    return res.status(401).res.json({ message: "権限がありません" });
+    return res.status(401).json({ message: "権限がありません" });
   }
 
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
     if (err) {
-      return res.status(401).res.json({ message: "権限がありません" });
+      return res.status(401).json({ message: "権限がありません" });
     }
 
     req.userId = decoded.id;
